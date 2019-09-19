0ca56069c0177c99b4c7ee3a38c12e6273dea9d4
venugopal009123 <55550123+venugopal009123@users.noreply.github.com>
2019-09-19 22:48:49 +0530

    Add files via upload

---
+++ ViewWelcomeController.java
@@ -0,0 +1,22 @@
+package com.fwmvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class ViewWelcomeController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mv=null;
+		mv=new ModelAndView();
+		mv.setViewName("welcome");
+
+		return mv;
+		
+	}
+	
+
+}
